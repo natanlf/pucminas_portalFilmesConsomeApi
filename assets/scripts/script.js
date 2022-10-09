8fd968be4ad248c136942f3a0f2b76e729922cff
natanlf <natanlaraferreira@gmail.com>
2022-10-09 15:08:37 -0300

    Busca filmes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,10 +3,45 @@ const API_CONFIG = {
     baseUrlSlideImage: "https://www.themoviedb.org/t/p/original/",
     baseUrlCardImage: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/",
     apiKey: `851d30eff2dc450ca53a76e2de8d0582`,
+    buscaEndpoint: "search/movie/",
     popularesEndpoint: "movie/popular/",
-    ultimosEndpoint: "movie/latest",
     cinemaEndpoint: "movie/now_playing/",
     language: "&language=pt-Br"
+}
+
+function pesquisarFilme() {
+  filmeBuscado = document.getElementById("buscarFilme").value; 
+  fetch(`${API_CONFIG.baseUrl}${API_CONFIG.buscaEndpoint}?api_key=${API_CONFIG.apiKey}${API_CONFIG.language}&query=${filmeBuscado}`)
+  .then(resp => resp.json())
+  .then(data => {
+
+    let buscaFilmeId = document.getElementById("buscaFilme");
+    buscaFilmeId.classList.remove("invisible");
+    buscaFilmeId.classList.add("visible");
+    buscaFilmeId.classList.add("p-4");
+
+    let concatString=`<h2 class="text-center">Busca Filmes</h2>
+    <div id="container-busca" class="d-flex flex-wrap justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-between justify-content-xl-between">`;
+      data.results.forEach(filme => {
+        concatString += `<div class="card" style="width: 18rem;">
+          <img src="${API_CONFIG.baseUrlCardImage}${filme.poster_path}" class="card-img-top" alt="${filme.original_title}">
+            <div class="card-body">
+              <h5 class="card-title">${filme.original_title}</h5>
+              <p class="card-text">
+              <p>${filme.release_date}</p>
+              <p>Média de Votos: <span class="badge rounded-pill bg-primary">${filme.vote_average}</span></p>
+              <p>Total Votos: <span class="badge rounded-pill bg-secondary">${filme.vote_count}</span></p>
+            </div>
+          </div>`;
+      })    
+      concatString += `</div>`;
+
+      document.getElementById("buscaFilme").innerHTML = concatString;
+  }).catch(error => {
+    console.log(error)
+    document.getElementById("container-populares").innerHTML = `<div class="alert alert-danger" role="alert">
+      Erro - Problemas ao consumir a api</div>`
+  })
 }
 
 function getPopulares() {
@@ -14,52 +49,21 @@ function getPopulares() {
         .then(resp => resp.json())
         .then(data => {
 
-            let concatString="";
-            data.results.forEach(filme => {
-                concatString += `<div class="card" style="width: 18rem;">
-                <img src="${API_CONFIG.baseUrlCardImage}${filme.poster_path}" class="card-img-top" alt="${filme.original_title}">
-                <div class="card-body">
-                  <h5 class="card-title">${filme.original_title}</h5>
-                  <p class="card-text">
-                  <p>${filme.release_date}</p>
-                  <p>Média de Votos: <span class="badge rounded-pill bg-primary">${filme.vote_average}</span></p>
-                  <p>Total Votos: <span class="badge rounded-pill bg-secondary">${filme.vote_count}</span></p>
-                </div>
-              </div>`;
-
-            
-            })    
-
-            document.getElementById("container-populares").innerHTML = concatString;
-        }).catch(error => {
-          console.log(error)
-          document.getElementById("container-populares").innerHTML = `<div class="alert alert-danger" role="alert">
-            Erro - Problemas ao consumir a api</div>`
-        })
-}
-
-function getUltimos() {
-    fetch(`${API_CONFIG.baseUrl}${API_CONFIG.ultimosEndpoint}?api_key=${API_CONFIG.apiKey}${API_CONFIG.language}`)
-        .then(resp => resp.json())
-        .then(data => {
-          
-          let img = "";
-          if(data.poster_path != null || data.poster_path != undefined) {
-            img = `<img src="${API_CONFIG.baseUrlCardImage}${data.poster_path}" class="card-img-top" alt="${data.original_title}">`;
-          }
-
           let concatString="";
-          concatString += `<div class="card" style="width: 18rem;">
-          ${img}
-            <div class="card-body">
-              <h5 class="card-title">${data.original_title}</h5>
-              <p class="card-text">
-              <p>${data.release_date}</p>
-            </div>
-          </div>`;
+          data.results.forEach(filme => {
+              concatString += `<div class="card" style="width: 18rem;">
+              <img src="${API_CONFIG.baseUrlCardImage}${filme.poster_path}" class="card-img-top" alt="${filme.original_title}">
+              <div class="card-body">
+                <h5 class="card-title">${filme.original_title}</h5>
+                <p class="card-text">
+                <p>${filme.release_date}</p>
+                <p>Média de Votos: <span class="badge rounded-pill bg-primary">${filme.vote_average}</span></p>
+                <p>Total Votos: <span class="badge rounded-pill bg-secondary">${filme.vote_count}</span></p>
+              </div>
+            </div>`;
+          })    
 
-          document.getElementById("container-ultimos").innerHTML = concatString;
-            
+          document.getElementById("container-populares").innerHTML = concatString;
         }).catch(error => {
           console.log(error)
           document.getElementById("container-populares").innerHTML = `<div class="alert alert-danger" role="alert">
@@ -72,22 +76,22 @@ function getCinema() {
         .then(resp => resp.json())
         .then(data => {
 
-            let concatString="";
-            data.results.forEach(filme => {
-                concatString += `<div class="card" style="width: 18rem;">
-                <img src="${API_CONFIG.baseUrlCardImage}${filme.poster_path}" class="card-img-top" alt="${filme.original_title}">
-                <div class="card-body">
-                  <h5 class="card-title">${filme.original_title}</h5>
-                  <p class="card-text">
-                  <p>${filme.release_date}</p>
-                  <p>Média de Votos: <span class="badge rounded-pill bg-primary">${filme.vote_average}</span></p>
-                  <p>Total Votos: <span class="badge rounded-pill bg-secondary">${filme.vote_count}</span></p>
-           
-                </div>
-              </div>`;
-            })    
+          let concatString="";
+          data.results.forEach(filme => {
+              concatString += `<div class="card" style="width: 18rem;">
+              <img src="${API_CONFIG.baseUrlCardImage}${filme.poster_path}" class="card-img-top" alt="${filme.original_title}">
+              <div class="card-body">
+                <h5 class="card-title">${filme.original_title}</h5>
+                <p class="card-text">
+                <p>${filme.release_date}</p>
+                <p>Média de Votos: <span class="badge rounded-pill bg-primary">${filme.vote_average}</span></p>
+                <p>Total Votos: <span class="badge rounded-pill bg-secondary">${filme.vote_count}</span></p>
+          
+              </div>
+            </div>`;
+          })    
 
-            document.getElementById("container-cinema").innerHTML = concatString;
+          document.getElementById("container-cinema").innerHTML = concatString;
         }).catch(error => {
           console.log(error)
           document.getElementById("container-populares").innerHTML = `<div class="alert alert-danger" role="alert">
@@ -95,9 +99,6 @@ function getCinema() {
         })
 }
 
-console.log(`${API_CONFIG.baseUrl}${API_CONFIG.popularesEndpoint}?api_key=${API_CONFIG.apiKey}${API_CONFIG.language}`)
-
 getPopulares();
-getUltimos();
 getCinema();
 
