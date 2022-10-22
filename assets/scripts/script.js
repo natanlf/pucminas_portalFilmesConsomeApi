3dd92e657f0f2fd6a930c7659a4cccb139263c68
natanlf <natanlaraferreira@gmail.com>
2022-10-21 21:31:24 -0300

    Melhorias scroll e cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -26,26 +26,30 @@ function pesquisarFilme(page = API_CONFIG.page) {
     buscaFilmeId.classList.add("p-4"); 
 
     let concatString=`<h2 class="text-center">Filme buscado: <b>${filmeBuscado}</b></h2>
-    <div id="container-busca" class="row flex-row flex-nowrap">`;
+    <div id="container-busca" class="row flex-row flex-nowrap">
+      <ion-icon id="back-arrow" name="chevron-back-outline" size="large"></ion-icon>`;
       data.results.forEach(filme => {
         concatString += `<div class="card">
           <img src="${API_CONFIG.baseUrlCardImage}${filme.poster_path}" class="card-img-top" alt="${filme.original_title}">
-            <div class="card-body">
+            <div class="card-body d-flex flex-column justify-content-around">
               <h5 class="card-title">${filme.original_title}</h5>
-              <p class="card-text">
-              <p>${filme.release_date}</p>
-              <p>Média de Votos: <span class="badge rounded-pill bg-primary">${filme.vote_average}</span></p>
-              <p>Total Votos: <span class="badge rounded-pill bg-secondary">${filme.vote_count}</span></p>
+              <div class="card-text d-flex flex-wrap justify-content-between">
+                ${convertPtBrDate(filme.release_date)}
+                ${buildMediaDeVotos(filme.vote_average, filme.vote_count)}
+              </div>
             </div>
           </div>`;
       })    
-      concatString += `</div>`;
+      concatString += `
+        <ion-icon id="next-arrow" name="chevron-forward-outline" size="large"></ion-icon>
+      </div>`;
 
       buscaFilmeId.innerHTML = concatString;
 
       if(data.total_pages > 1) {
         buildPagination(data.total_pages);
       }
+      arrowBackNext();
   }).catch(error => {
     console.log(error)
     document.getElementById("container-populares").innerHTML = `<div class="alert alert-danger" role="alert">
@@ -67,9 +71,8 @@ function getPopulares() {
               <div class="card-body">
                 <h5 class="card-title">${filme.original_title}</h5>
                 <p class="card-text">
-                <p>${filme.release_date}</p>
-                <p>Média de Votos: <span class="badge rounded-pill bg-primary">${filme.vote_average}</span></p>
-                <p>Total Votos: <span class="badge rounded-pill bg-secondary">${filme.vote_count}</span></p>
+                <p>${convertPtBrDate(filme.release_date)}</p>
+                ${buildMediaDeVotos(filme.vote_average, filme.vote_count)}
               </div>
             </div>`;
           })    
@@ -111,9 +114,6 @@ function getCinema() {
         })
 }
 
-getPopulares();
-getCinema();
-
 function buildItemPage(page) {
   if(API_CONFIG.page === page) {
     return `<li onclick="pesquisarFilme(${page})" class="page-item"><a class="page-link active-custom-page" href="#">${page}</a></li>`;
@@ -148,3 +148,44 @@ function buildPagination(totalPages) {
         containerBusca.insertAdjacentHTML("afterend", htmlPagination);
 }
 
+function buildMediaDeVotos(media, totalVotos) {
+  if(media) {
+    let classMediaVotos = "";
+    if(media >= 7.0) {
+      classMediaVotos = "media-votos-top";
+    } else if(media >= 6.0 && media < 7.0) {
+      classMediaVotos = "media-votos-medio";
+    } else {
+      classMediaVotos = "media-votos-baixo";
+    }
+    return `<span class="badge rounded-pill bg-primary media-votos ${classMediaVotos}">${media*10}</span>`;
+  }
+}
+
+function arrowBackNext() {
+  document.getElementById("back-arrow").addEventListener("click", () => {
+    document.getElementById("container-busca").scrollLeft -= 290;
+  });
+
+  document.getElementById("next-arrow").addEventListener("click", () => {
+    document.getElementById("container-busca").scrollLeft += 290;
+  });
+}
+
+function convertPtBrDate(date) {
+  if(date) {
+    let dateArray = date.split("-");
+    return `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`
+  }
+}
+
+function scrollBarIsVisible() {
+  
+}
+
+function init() {
+  getPopulares();
+  getCinema();
+}
+
+init();
